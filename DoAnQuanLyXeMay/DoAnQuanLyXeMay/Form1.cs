2daf62c02f41d85ffb3466336af800f4a3bdd075
HuynhVietHoang <102209757+HuynhVietHoang@users.noreply.github.com>
2022-03-23 15:05:28 +0700

    test again

--- DoAnQuanLyXeMay/DoAnQuanLyXeMay/Form1.cs
+++ DoAnQuanLyXeMay/DoAnQuanLyXeMay/Form1.cs
@@ -24,7 +24,7 @@ namespace DoAnQuanLyXeMay
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(label1.Text);
+            MessageBox.Show(textBox1.Text);
         }
         //anh iu em
     }
